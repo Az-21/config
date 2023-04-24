8a66580ff323437a715d48d25ae8bfc76e559c64
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-24 22:45:49 +0530

    Add `yt-dlp` alias and comments

--- terminal/powershell/profile.ps1
+++ terminal/powershell/profile.ps1
@@ -1,12 +1,12 @@
 # Alias first party tools and commands
-New-Alias e Explorer # Open explorer
-New-Alias hash Get-FileHash # Generate SHA256 hash
-New-Alias touch New-Item # Create new, empty file
+New-Alias e Explorer              # Open explorer
+New-Alias hash Get-FileHash       # Generate SHA256 hash
+New-Alias touch New-Item          # Create new, empty file
 
 # Alias third party tools and commands
-New-Alias vi nvim
-New-Alias vim nvim
+New-Alias vi nvim                 # NeoVim
+New-Alias vim nvim                # NeoVim
+New-Alias dl yt-dlp               # YouTube Downloader
 
 # Instantiate oh-my-posh 
 oh-my-posh init pwsh --config "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\amro.omp.json" | Invoke-Expression
-
